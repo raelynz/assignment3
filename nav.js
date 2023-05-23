0f707703e97210aaeaf9c9b285b03c3dddb4a91c
julcheng <405702216abc@gmail.com>
2023-05-24 04:01:57 +1000

    nav.js

---
+++ nav.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var searchInput = document.querySelector('.search_bar');
+    searchInput.addEventListener('focus', function() {
+      this.placeholder = '';
+    });
+    searchInput.addEventListener('blur', function() {
+      if (!this.value) {
+        this.placeholder = 'What are you looking for?';
+      }
+    });
+  });
+
+
+  
